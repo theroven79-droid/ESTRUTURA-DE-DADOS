16ccc191ff4fc8f0d842beb1ed73289ffd9bdf86
theroven79-droid <theroven79@gmail.com>
2026-09-23 10:18:45 -0300

    update codigo

--- programa-5-biblioteca/main.cpp
+++ programa-5-biblioteca/main.cpp
@@ -2,13 +2,19 @@
 
 int main()
 {
-    int a, b;
-    a = 30;
-    b = 20;
+    int a, b; 
 
-    printf("\t|SOMA: %d\n", soma(a, b));
+    printf("Digite o valor de a: ");
+    scanf("%d", &a);
+
+    printf("Digite o valor de b: ");
+    scanf("%d", &b);
+
+    printf("\n\t|SOMA: %d\n", soma(a, b));
     printf("\t|SUBTRACAO: %d\n", subtracao(a, b));
     printf("\t|MULTIPLICACAO: %d\n", multiplicacao(a, b));
     printf("\t|DIVISAO: %.2f\n", divisao(a, b));
     printf("\t|CUBO: %d\n", cubo(a, b));
+
+    return 0;
 }
